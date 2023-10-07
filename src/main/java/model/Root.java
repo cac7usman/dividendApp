d1e49dbdeacb503ccb4207eb2eb66e6386d58960
Alexander Shumilovsky <a.shumil@gmail.com>
2023-10-07 20:52:18 +0200

    polygon response model

---
+++ src/main/java/model/Root.java
@@ -0,0 +1,15 @@
+package model;
+import java.util.ArrayList;
+
+public class Root{
+    public ArrayList<Result> results;
+    public String status;
+    public String request_id;
+    public String next_url;
+    public String ticker;
+    public int queryCount;
+    public int resultsCount;
+    public boolean adjusted;
+    public int count;
+
+}
