9d1c07145cad9f7bf399baeddc9b0a4cb748b44d
Alexander Shumilovsky <a.shumil@gmail.com>
2023-10-17 09:47:54 +0200

    result model

---
+++ src/main/java/model/Result.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Result{
+    public double cash_amount;
+    public String currency;
+    public String declaration_date;
+    public String dividend_type;
+    public String ex_dividend_date;
+    public int frequency;
+    public String pay_date;
+    public String record_date;
+    public String ticker;
+}
